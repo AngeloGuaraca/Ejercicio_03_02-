58e720c6c82156293324b92945786e49e3a0a4b5
ROG PC <ROG PC@192.168.1.7>
2022-05-20 03:31:41 -0500

    Ejercicio_03_02

---
+++ src/main/java/ejercicio03_02/Inicio.java
@@ -0,0 +1,20 @@
+package ejercicio03_02;
+
+
+/**
+ *
+ *
+ */
+public class Inicio {
+
+    public static void main(String[] args) {
+        
+     
+        
+        var v4 = new Ventana3 ("INFORMACION DE CARRERA ");
+        v4.setVisible(true);
+        
+        var v5 = new Ventana4 ("CREAR GRUPO ");
+        v5.setVisible(true);
+    }
+}
